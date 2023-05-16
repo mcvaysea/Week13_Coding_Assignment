0274dff4c95dec63fc44a26569898220b5ddc248
mcvaysea <mcvay.sean@gmail.com>
2023-05-16 09:54:47 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+let date = new Date();
+
+let initUser = {
+	name: "User",
+	date: date.toDateString(),
+	time: date.toLocaleTimeString(),
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<nav>
+				<ul>
+					<li>Home</li>
+					<li>Create Login</li>
+					<li>Settings</li>
+					<li>Contact</li>
+				</ul>
+			</nav>
+      <userInfo />
+			<section className="welcomeSection">
+				<h1>Hello {initUser.name}, and Welcome to React</h1>
+				<p>Todays date is: {initUser.date}</p>
+				<p>The time is now: {initUser.time}</p>
+			</section>
+		</div>
+	);
 }
 
+function userInfo() {
+	return (
+		<div>
+			<label for="text-form">username</label>
+			<form type="text" id="text-form"></form>
+		</div>
+	);
+}
+
+
 export default App;
+
